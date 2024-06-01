9713017fcbcaec08286353787753962bc181456d
AdhilKappil <adhilalikappil@gmail.com>
2024-06-01 14:32:26 +0530

    update:build file

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ export const useSocket = (): Socket | null => useContext(SocketContext);
 function App() {
 
   const socket = useMemo(()=>{
-    return io("http://localhost:3000");
+    return io("http://shopbrandhub.online");
   },[])
 
   return (
